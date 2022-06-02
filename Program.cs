2ae0640af9bd9c24535a95c0e6045bb357cf0a14
Shvaiko Pavlo <shvaikop02@gmail.com>
2022-06-02 17:11:54 +0200

    REMOVE logic changed to recursive, tests added

--- Program.cs
+++ Program.cs
@@ -69,43 +69,44 @@ class AVL_TREE<TK, TV> where TK:IComparable
             throw new System.Exception("Not a child");
     }
 
-    public bool Remove(TK key)
-    {
-        var n = this._root;
-        Node<TK, TV>? parent = null;
-        
-        while (n != null) {
-            if (key.CompareTo(n.Key) < 0) {
-                parent = n;
-                n = n.Left;
-            }
-            else if (key.CompareTo(n.Key) > 0) {
-                parent = n;
-                n = n.Right;
-            }
+    private Node<TK, TV> remove_help(Node<TK, TV> n, TK key) {
+        if (n == null) {
+            throw new System.Exception("Key to be deleted does not exist");
+        }
+        if (key.CompareTo(n.Key) < 0) {
+            n.Left = this.remove_help(n.Left, key);
+        }
+        else if (key.CompareTo(n.Key) > 0) {
+            n.Right = this.remove_help(n.Right, key);
+        }
+        else
+        {
+            if (n.Left == null && n.Right == null)
+                return null;
+            else if (n.Left == null && n.Right != null)
+                return n.Right;
+            else if (n.Left != null && n.Right == null)
+                return n.Left;
             else
             {
-                if (n.Left == null && n.Right == null)
-                    this.Replace(parent, n, null);
-                else if (n.Left == null && n.Right != null)
-                    this.Replace(parent, n, n.Right);
-                else if (n.Left != null && n.Right == null)
-                    this.Replace(parent, n, n.Left);
-                else
-                {
-                    Node<TK, TV> temp_node = n.Right;
-                    while (temp_node.Left != null)
-                        temp_node = temp_node.Left;
-                    TK temp_key = temp_node.Key;
-                    TV temp_val = temp_node.Val;
-                    this.Remove(temp_node.Key);
-                    n.Key = temp_key;
-                    n.Val = temp_val;
-                }
-                return true;
+                Node<TK, TV> temp_node = n.Right;
+                while (temp_node.Left != null)
+                    temp_node = temp_node.Left;
+                TK temp_key = temp_node.Key;
+                TV temp_val = temp_node.Val;
+                this.Remove(temp_node.Key);
+                n.Key = temp_key;
+                n.Val = temp_val;
             }
         }
-        return false;
+        return n;
+    }
+
+    public bool Remove(TK key)
+    {
+        // first call contains to see if key exists
+        this._root = this.remove_help(this._root, key);
+        return true;
     }
 }
 
@@ -113,12 +114,12 @@ class Program
 {
     static void Main()
     {
-        test_1();
+        // test_1();
+        
     }
     
     // recursive bst deletion
-    static void test_1()
-    {
+    static void test_1() {
         AVL_TREE<int, string> tree = new AVL_TREE<int, string>();
         tree.Add(50, "hello");
         tree.Add(30, "hello");
@@ -130,7 +131,24 @@ class Program
         tree.Add(65, "hello");
         
         tree.printInorder();
-        tree.Remove(50);
+        tree.Remove(30);
+        tree.printInorder();
+    }
+
+    static void test_2() {
+        AVL_TREE<int, string> tree = new AVL_TREE<int, string>();
+        Random r = new Random(200);
+        for (int i = 0; i < 50; i++)
+        {
+            int num = r.Next(1000);
+            tree.Add(num, "hello");
+        }
+        tree.printInorder();
+        tree.Remove(22);
+        tree.Remove(122);
+        tree.Remove(246);
+        tree.Remove(720);
+        tree.Remove(934);
         tree.printInorder();
     }
 }
